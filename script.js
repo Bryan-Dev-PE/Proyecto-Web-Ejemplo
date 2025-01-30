5d114ea70ee79ed67e55bf3313662c4af98081ee
Bryan <126097741+Bryan-Dev-PE@users.noreply.github.com>
2025-01-29 22:06:32 -0500

    Añadiendo la base del Proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Función para mostrar un mensaje de bienvenida en la consola
+function mostrarMensajeBienvenida() {
+    console.log("¡Bienvenido a mi Proyecto Web!");
+}
+
+// Llamar a la función para mostrar el mensaje
+mostrarMensajeBienvenida();
